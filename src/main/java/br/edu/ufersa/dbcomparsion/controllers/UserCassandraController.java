fe51a6b7b1d8dd58a0b6283ee662b958bc3e724d
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-10-08 16:16:28 -0300

    cassandra e mongo controller

---
+++ src/main/java/br/edu/ufersa/dbcomparsion/controllers/UserCassandraController.java
@@ -0,0 +1,43 @@
+package br.edu.ufersa.dbcomparsion.controllers;
+
+import br.edu.ufersa.dbcomparsion.cassandra.UserCassandra;
+import br.edu.ufersa.dbcomparsion.cassandra.UserCassandraService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api/users/cassandra")
+public class UserCassandraController {
+    private final UserCassandraService userCassandraService;
+    public UserCassandraController(UserCassandraService userCassandraService) {
+        this.userCassandraService = userCassandraService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save (
+            @RequestBody UserCassandra user
+    ) {
+        return ResponseEntity.ok(
+                userCassandraService.save(user)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable String id){
+        return ResponseEntity.ok(
+                userCassandraService.getById(id)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(
+            @PathVariable String id,
+            @RequestBody UserCassandra user
+    ) {
+        return ResponseEntity.ok(userCassandraService.update(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        userCassandraService.deleteById(id);
+    }
+}
